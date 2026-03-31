f53d2aa995866ef9c600ddc1e1dc0a44ddeacc6b
akhmet-tolegen07 <ak_tolegen@kbtu.kz>
2026-03-31 23:49:30 +0500

    m

---
+++ Lab9/shop-back/shop-back/api/views.py
@@ -0,0 +1,23 @@
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'], url_path='products')
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
